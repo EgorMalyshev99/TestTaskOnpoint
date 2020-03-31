5857c7eca8db135e9afb0061583b9a5b71fedb1a
EgorMalyshev99 <egor.mlshv99@mail.ru>
2020-03-31 17:14:51 +0300

    Updates

---
+++ precache-manifest.646471a5077e4911b66d6426c5e9d55d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c7870973f34809a95f9d0844d9bbd7",
+    "url": "/TestTaskOnpoint/index.html"
+  },
+  {
+    "revision": "ea858c838ad44a1bbed7",
+    "url": "/TestTaskOnpoint/static/css/main.27012b35.chunk.css"
+  },
+  {
+    "revision": "9302edbac001e02b3077",
+    "url": "/TestTaskOnpoint/static/js/2.7799a2e2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/TestTaskOnpoint/static/js/2.7799a2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea858c838ad44a1bbed7",
+    "url": "/TestTaskOnpoint/static/js/main.3997055b.chunk.js"
+  },
+  {
+    "revision": "dfffc404137f7c4cbb3b",
+    "url": "/TestTaskOnpoint/static/js/runtime-main.d017fa50.js"
+  },
+  {
+    "revision": "0b63986f9429b51b64c29c86c9f4f277",
+    "url": "/TestTaskOnpoint/static/media/bg_slide1.0b63986f.png"
+  },
+  {
+    "revision": "b30a72d841b6176ce2be21fd79f8e255",
+    "url": "/TestTaskOnpoint/static/media/bg_slide2.b30a72d8.png"
+  },
+  {
+    "revision": "acc30976e6a8a3f19f8b540c77f469b1",
+    "url": "/TestTaskOnpoint/static/media/bg_slide3.acc30976.png"
+  },
+  {
+    "revision": "a0f27b16d42f1ca6214400d6656780cd",
+    "url": "/TestTaskOnpoint/static/media/ice1_1.a0f27b16.png"
+  },
+  {
+    "revision": "f288af9f55dd85a2c4ad87fa74179f74",
+    "url": "/TestTaskOnpoint/static/media/ice1_2.f288af9f.png"
+  },
+  {
+    "revision": "8ed38d6220146ec692f6eda57e6380f5",
+    "url": "/TestTaskOnpoint/static/media/ice2_1.8ed38d62.png"
+  },
+  {
+    "revision": "e2d8896ccfd0b5a04ad2a7747e1ce281",
+    "url": "/TestTaskOnpoint/static/media/ice2_2.e2d8896c.png"
+  },
+  {
+    "revision": "43f5439489ff9dfe80fb930fd4eba5af",
+    "url": "/TestTaskOnpoint/static/media/ice3_1.43f54394.png"
+  },
+  {
+    "revision": "2f0f8db18718be654ce78a1c7814fc4d",
+    "url": "/TestTaskOnpoint/static/media/ice3_2.2f0f8db1.png"
+  },
+  {
+    "revision": "880af91145f6989141f524631414c4a1",
+    "url": "/TestTaskOnpoint/static/media/next.880af911.png"
+  },
+  {
+    "revision": "9670ee6ae5b72d3bc5ca71da2b8d9b6a",
+    "url": "/TestTaskOnpoint/static/media/scheme1.9670ee6a.png"
+  },
+  {
+    "revision": "e633a5a75d0f7808e18299d7424885c4",
+    "url": "/TestTaskOnpoint/static/media/scheme2.e633a5a7.png"
+  },
+  {
+    "revision": "d5023f7970de93fc75e7e8532cbda888",
+    "url": "/TestTaskOnpoint/static/media/scheme3.d5023f79.png"
+  },
+  {
+    "revision": "9e773fdec5f600db6742ea9cc75d89a6",
+    "url": "/TestTaskOnpoint/static/media/slide2_ice2.9e773fde.png"
+  },
+  {
+    "revision": "04b0cc450aeca282bb1263438ef8b63a",
+    "url": "/TestTaskOnpoint/static/media/slide2_ice4.04b0cc45.png"
+  }
+]);
